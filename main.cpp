ca412ff7d0a3ad652f3d506d7f5b526b7e7f5aeb
lthornton1 <lthornton1@mail.csuchico.edu>
2018-03-28 18:20:38 -0700

    forgot to make it look for the letters

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
   ifstream in("example.txt");
   char first = ' ', last = ' ', current;
   vector<char> file;
-
+  int i=1;
   if (in.is_open())
   {
     while (!in.eof())
@@ -18,17 +18,24 @@ int main()
       file.push_back(current);
     }
   }
+
   first = file[0];
-  last = file[file.size()-1];
+
+  while (!isalpha(file[file.size()-i]))
+  {
+    i++;
+  }
+
+  last = file[file.size()-i];
   in.close();
   //Read from the file opened by in, and the first letter (either upper or
   //lower case) should be assigned to first and the last letter (again in
   //either case) from the file to the variable last
 
+  cout<<first<<"**"<<last<<endl;
 
 
   //Then, finally, this displays the censored version:
-  cout<<first<<"**"<<last<<endl;
 
   return 0;
 }
