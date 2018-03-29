488367a3610e4f0a579a776814d1ff42ccd3228b
lthornton1 <lthornton1@mail.csuchico.edu>
2018-03-28 18:08:07 -0700

    oh i see what you did there

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 //Author:
 #include <iostream>
 #include <fstream>
-
+#include<vector>
 using namespace std;
 
 int main()
 {
   ifstream in("example.txt");
   char first = ' ', last = ' ', current;
+  vector<char> file;
 
+  if (in.is_open())
+  {
+    while (!in.eof())
+    {
+      in>>current;
+      file.push_back(current);
+    }
+  }
+  first = file[0];
+  last = file[file.size()-1];
+  in.close();
   //Read from the file opened by in, and the first letter (either upper or
   //lower case) should be assigned to first and the last letter (again in
   //either case) from the file to the variable last
